ad64393d6577baa78bb64fc0bb2490f49d1dc688
ndemeyvan <ndemeyvan@gmail.com>
2020-12-27 09:34:08 +0100

    commit change ,apply mvc structure to our node serve

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const { json } = require("express");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const blogModel = require("./models/blogModel");
+const blogRoute= require('./Routes/BlogRoute');
+
 
 mongoose.connect(
   "mongodb://localhost/blog",
@@ -12,8 +13,6 @@ mongoose.connect(
       console.log("Express ecoute sur le port 3000");
     });
   }
-  //   { useNewUrlParser: true },
-  //   { useUnifiedTopology: true }
 );
 
 app.get("/", (req, res) => {
@@ -21,34 +20,8 @@ app.get("/", (req, res) => {
   res.send("Welcome to Blog server");
 });
 
-app.get("/blog", (req, res) => {
-  const blog = new blogModel({
-    title: "Full stack web dev",
-    snippet: "Hello world",
-    body:
-      "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum.",
-  });
 
-  blog.save((error, doc) => {
-    if (error) {
-      console.log(error);
-    } else {
-      res.send(doc);
-      console.log("This is doc : ", doc);
-    }
-  });
-});
-
-app.get("/all-blogs", (req, res) => {
-  blogModel.find((err, doc) => {
-    if (err) {
-      console.log(err);
-      res.send(err);
-    } else {
-      res.send(doc);
-    }
-  });
-});
+app.use('/blog',blogRoute)
 
 // app.get("/about", (req, res) => {
 //   res.send("Welcome to about page");
