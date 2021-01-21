4986f3063c25b798793722487db0395655ab4b32
ndemeYvan <ndemeyvan@gmail.com>
2021-01-21 20:01:58 +0100

    some change

--- nodejsNinja/Routes/BlogRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const blogController = require("../controller/blogController");
-
-router.get("/", blogController.blog_index);
-
-router.get("/all-blogs", blogController.blog_allBlog);
-
-module.exports = router;
